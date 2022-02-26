5ef460e204a99b00e04b450c635e78f7172fc614
imrnekko <63248465+imrnekko@users.noreply.github.com>
2022-02-26 11:34:01 +0800

    Delete model directory

--- model/contacts.dart
+++
@@ -1,9 +0,0 @@
-import 'package:cloud_firestore/cloud_firestore.dart';
-
-class UserContacts {
-  String? user;
-  String? phone;
-  DateTime? checkin;
-
-  UserContacts({this.user, this.phone, this.checkin});
-}
